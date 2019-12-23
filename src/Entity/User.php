624d217be0d153993c143b44dc2f4a6e9b41d9a5
sorelgd <sorel.godonoudossou@gmail.com>
2019-12-23 17:15:02 +0100

    first init

---
+++ src/Entity/User.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creationdate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedate;
+
+    public function __toString()
+    {
+        if(is_null($this->getFirstname().' '.$this->getLastname)) {
+            return 'NULL';
+        }
+
+        return $this->getTitle();
+    }
+
+    public function __construct()
+    {
+        $this->creationdate= new \Datetime("now", new \DateTimeZone('UTC'));
+        $this->updatedate = new \Datetime("now", new \DateTimeZone('UTC'));
+
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getCreationdate(): ?\DateTimeInterface
+    {
+        return $this->creationdate;
+    }
+
+    public function setCreationdate(\DateTimeInterface $creationdate): self
+    {
+        $this->creationdate = $creationdate;
+
+        return $this;
+    }
+
+    public function getUpdatedate(): ?\DateTimeInterface
+    {
+        return $this->updatedate;
+    }
+
+    public function setUpdatedate(\DateTimeInterface $updatedate): self
+    {
+        $this->updatedate = $updatedate;
+
+        return $this;
+    }
+}
